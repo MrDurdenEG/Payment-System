92aca5688948f2e24a45ac44591a868134f88b17
Yousef Elnagar <121249261+MrDurdenEG@users.noreply.github.com>
2024-08-24 03:36:13 +0300

    Update server.h

--- system/Payment_system/Server/server.h
+++ system/Payment_system/Server/server.h
@@ -47,11 +47,19 @@ typedef struct ST_accountsDB_t
     uint8_t primaryAccountNumber[20];
 } ST_accountsDB_t;
 
+typedef struct AccountNode {
+    ST_accountsDB_t account;
+    struct AccountNode* next;
+} AccountNode;
+
+extern AccountNode* head ;
+
 extern int index ;
+
 EN_transState_t recieveTransactionData(ST_transaction_t *transData,ST_cardData_t *cardData,ST_terminalData_t *termData);
 EN_serverError_t isValidAccount(ST_cardData_t *cardData);
-EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountRefrence);
-EN_serverError_t isAmountAvailable(ST_terminalData_t *termData);
+EN_serverError_t isBlockedAccount(ST_cardData_t *cardData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t *termData,ST_cardData_t *cardData);
 EN_serverError_t saveTransaction(ST_transaction_t *transData,ST_terminalData_t *termData,ST_cardData_t *cardData);
 void listSavedTransactions(void);
 
