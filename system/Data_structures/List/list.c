c5711f40f528ad3f872fb01aece5dda5163c4cec
Yousef Elnagar <121249261+MrDurdenEG@users.noreply.github.com>
2024-08-24 03:33:01 +0300

    list.c

--- system/Data_structures/List/list.c
+++ system/Data_structures/List/list.c
@@ -1,1 +1,64 @@
 
+#include "../../Libraries/Libraries.h"
+
+ AccountNode* head = NULL;
+void addAccountNode(ST_accountsDB_t account) {
+    AccountNode* newNode = (AccountNode*)malloc(sizeof(AccountNode));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    newNode->account = account;
+    newNode->next = head;
+    head = newNode;
+}
+
+AccountNode* findAccountByPAN(const char* pan) {
+    AccountNode* current = head;
+    while (current != NULL) {
+        if (strcmp(current->account.primaryAccountNumber, pan) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void deleteAccountNode(const char* pan) {
+    AccountNode* current = head;
+    AccountNode* previous = NULL;
+    while (current != NULL && strcmp(current->account.primaryAccountNumber, pan) != 0) {
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL) return;
+    if (previous == NULL) {
+        head = current->next;
+    } else {
+        previous->next = current->next;
+    }
+    free(current);
+}
+
+void freeLinkedList() {
+    AccountNode* current = head;
+    while (current != NULL) {
+        AccountNode* next = current->next;
+        free(current);
+        current = next;
+    }
+    head = NULL;
+}
+
+AccountNode* reverseList(AccountNode* head) {
+    AccountNode* prev = NULL;
+    AccountNode* current = head;
+    AccountNode* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
