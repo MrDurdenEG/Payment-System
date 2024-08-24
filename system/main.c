c47795cbe295d21eeeb5ad87b55b2ae16c377bf2
Yousef Elnagar <121249261+MrDurdenEG@users.noreply.github.com>
2024-08-24 03:37:10 +0300

    Update main.c

--- system/main.c
+++ system/main.c
@@ -1,14 +1,15 @@
 int main(void)
 {
-    
     system("cls");
 
     printf("====================================\n");
     printf("\033[1;36m   WELCOME TO THE TRANSACTION SYSTEM\033[0m\n");
     printf("====================================\n\n");
 
+
     printf("\033[1;36mInitializing the application...\033[0m\n\n");
     appStart();
+    freeLinkedList();
 
     printf("\n====================================\n");
     printf("\033[1;36m  Thank you for using our system!\033[0m\n");
