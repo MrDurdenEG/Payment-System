f54eebc5f2ac39253132753f6c42a46f0122c019
Yousef Elnagar <121249261+MrDurdenEG@users.noreply.github.com>
2024-08-24 03:33:17 +0300

    list.h

--- system/Data_structures/List/list.h
+++ system/Data_structures/List/list.h
@@ -1,1 +1,12 @@
 
+
+#ifndef LIST_H
+#define LIST_H
+AccountNode* reverseList(AccountNode* head);
+void addAccountNode(ST_accountsDB_t account);
+AccountNode* findAccountByPAN(const char* pan);
+void deleteAccountNode(const char* pan);
+void freeLinkedList(void);
+
+#endif // LIST_H
+
